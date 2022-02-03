b99693e14e3ab1466a534e7e11eb697295975f4f
makeit-mike <mike.isaac.jones@gmail.com>
2022-02-03 12:22:54 -0600

    adding in mouse sensitivity script

--- scripts/setuptp.sh
+++ scripts/setuptp.sh
@@ -1,12 +1,15 @@
 # rebinding CAPSlock to Bksp
 setxkbmap -option caps:backspace
 
+# Setting touchpad options
+
+Mouse=`xinput list |grep -i Trackpad | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
+
 # macOS like scrolling
-xinput --set-prop 12 "libinput Natural Scrolling Enabled" 1
+xinput --set-prop $Mouse "libinput Natural Scrolling Enabled" 1
 
 # tap to click
-xinput --set-prop 12 "libinput Tapping Enabled" 1
+xinput --set-prop $Mouse "libinput Tapping Enabled" 1
 
-# Debugging
-# xinput list
-# xinput --list-props 12
+# Increase Mouse Sensitivity
+xinput set-prop $Mouse "libinput Accel Speed" .5
