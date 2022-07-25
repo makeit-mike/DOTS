f0205cd179174b4d3ae498a03031e8ae16642db2
michaelAtMercury <michaeljones@mercury.com>
2022-07-25 14:52:10 -0500

    adding qmk codes I use on every keyboard

---
+++ qmk.c
@@ -0,0 +1,6 @@
+
+#define KC_TMUX LCTL(KC_A)              // TMUX key 
+#define KC_CAPW LGUI(LSFT(KC_3))        // Capture whole screen
+#define KC_CPYW LGUI(LSFT(LCTL(KC_3)))  // Copy whole screen
+#define KC_CAPP LGUI(LSFT(KC_4))        // Capture portion of screen
+#define KC_CPYP LGUI(LSFT(LCTL(KC_4)))  // Copy portion of screen
